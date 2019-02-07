fb910c94f373da6ffe9f5364ad95a551a210d1e0
dvotaw <votaw@steelhead.nscl.msu.edu>
2019-02-07 10:42:31 -0500

    Added roundoff_distribution.cpp

--- roundoff_plotter.C
+++ roundoff_plotter.C
@@ -1,11 +1,12 @@
 //  File: roundoff_plotter.C
 //
-//  This is a macro to plot the results of roundoff.x in ROOT, rather than GNUPlot.
+//  This is a macro to plot the results of roundoff.x and roundoff_distribution.x in ROOT, rather than GNUPlot.
 //
 //  Programmer:  Daniel Votaw  votaw@nscl.msu.edu
 //
 //  Revision history:
 //      28-Jan-2019  Written from scratch.
+//      07-Feb-2019  Added second half for roundoff_distribution.x output.
 //
 //  Notes:  
 //   * Does not need to be compiled. Run in ROOT, from command line:
@@ -32,4 +33,27 @@ void roundoff_plotter(void)
   g->Fit("pol1", "", "", 4, 7);
 
   c->Print("roundoff.pdf", "pdf");
+
+  TCanvas* d = new TCanvas("d", "d");
+  d->cd();
+
+  Double_t temp = 0.;
+
+  ifstream infile;
+  infile.open("roundoff_dist.txt");
+
+  TH1D* h = new TH1D("h", "Roundoff error distribution", 100, -0.0000001, 0.0000001);
+  h->GetXaxis()->SetTitle("Relative error");
+  h->GetYaxis()->SetTitle("Counts");
+
+  while(!infile.eof())
+  {
+    infile >> temp;
+    h->Fill(temp);
+  }
+
+  h->Draw();
+  h->Fit("gaus");
+
+  d->Print("roundoff_dist.pdf", "pdf");
 }
